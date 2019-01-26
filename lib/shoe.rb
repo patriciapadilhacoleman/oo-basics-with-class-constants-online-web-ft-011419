1785dda97c4d4d2b2a581bccfcc2b534afd96b05
Patricia Padilha Coleman <patricia.p.coleman@gmail.com>
2019-01-26 21:02:58 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,7 +5,8 @@ class Shoe
   BRANDS = []
 
   def initialize(brand)
-    brand(brand)
+    @brand = brand
+    BRANDS << brand
   end
 
   def cobble
@@ -13,9 +14,6 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
   
-  def brand=(brand)
-    @brand = brand
-    BRANDS << brand
-  end
+ 
 
 end
